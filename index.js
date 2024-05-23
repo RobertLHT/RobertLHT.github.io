729f57deffc0348f42c31cacd40b802fd76fae82
Haotian <Robertlht0828@gmail.com>
2024-05-23 17:32:52 -0400

    all

---
+++ index.js
@@ -0,0 +1,7 @@
+// alert("linked");
+
+const access_key = "125f83f8-784e-455a-bec5-a49a0d192de1";
+
+document.getElementById("access_key").setAttribute("value", access_key); // = access_key;
+// alert(access_key);
+// console.log(document.getElementById("access_key"));
